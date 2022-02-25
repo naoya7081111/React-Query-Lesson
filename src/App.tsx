ebef16a193157313a449d4fbffa425a6dff61980
naoya7081111 <naoya7081111@gmail.com>
2022-02-25 11:32:05 +0900

    react-queryの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
 import { VFC } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query-devtools';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ClassicalFetchA } from './components/ClassicalFetchA';
 import { ClassicalFetchB } from './components/ClassicalFetchB';
 import { Layout } from './components/Layout';
+import { ReactQueryA } from './components/ReactQueryA';
+import { ReactQueryB } from './components/ReactQueryB';
 import { StateProvider } from './context/StateProvider';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,                 
+      refetchOnWindowFocus: false,  
+    }
+  }
+})
+//fetchに失敗したときにリトライするか　※デフォルトは３回
+//マウスをウィンドウに当てているときはfetchを自動で取りに行く設定 ※デフォルトはtrue
 
 const App: VFC = () => {
   return (
-    <BrowserRouter>
-      <StateProvider>
-        <Layout>
-          <Routes>
-            <Route path="/fetch-a" element={<ClassicalFetchA />} />
-            <Route path="/fetch-b" element={<ClassicalFetchB />} />
-          </Routes>
-        </Layout>
-      </StateProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <StateProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<ReactQueryA />} />
+              <Route path="/query-B" element={<ReactQueryB />} />
+              <Route path="/fetch-a" element={<ClassicalFetchA />} />
+              <Route path="/fetch-b" element={<ClassicalFetchB />} />
+            </Routes>
+          </Layout>
+        </StateProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
---
+++ src/components/ReactQueryA.tsx
@@ -0,0 +1,27 @@
+import { ChevronDoubleRightIcon } from '@heroicons/react/solid';
+import React, { VFC } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useQueryTasks } from '../hooks/useQueryTasks';
+
+export const ReactQueryA: VFC = () => {
+
+    const navigation = useNavigate();
+    const { status, data } = useQueryTasks();
+    console.log(data)
+
+    if(status === 'loading') return <div>{'Loading...'}</div>
+    if(status === 'error') return <div>{'Error'}</div>
+    return (
+        <>
+            <p className='font-bold my-3'>ReactQueryA</p>
+            {data?.map((task: any) => (
+                <p key={task.id}>{task.title}</p>
+            ))}
+            <ChevronDoubleRightIcon 
+            onClick={() => navigation('/query-b')} 
+            className="h-5 w-5 mt-2 text-blue-500 cursor-pointer" 
+            />
+            <p className='text-sm'>react-query B</p>
+        </>
+    )
+}
